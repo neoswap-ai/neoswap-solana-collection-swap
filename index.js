3167448e37549726670bc4073fb84a1cdc600d5e
biboux-neoswap <biboux.esmail@neoswap.ai>
2023-06-23 00:49:07 +0700

    spacing

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import {createInitializeSwapInstructions} from "./src/programInstructions/initializeSwap.instructions";
-import {initializeSwap} from "./src/processor/initializeSwap";
-import {getProgram} from "./src/utils/getProgram.obj";
-import {getSwapDataAccountFromPublicKey} from "./src/utils/getSwapDataAccountFromPublicKey.function";
-import {getSwapIdentityFromData} from "./src/utils/getSwapIdentityFromData.function";
-import {sendBundledTransactions} from "./src/utils/sendBundledTransactions.function";
+import { createInitializeSwapInstructions } from "./src/programInstructions/initializeSwap.instructions";
+import { initializeSwap } from "./src/processor/initializeSwap";
+import { getProgram } from "./src/utils/getProgram.obj";
+import { getSwapDataAccountFromPublicKey } from "./src/utils/getSwapDataAccountFromPublicKey.function";
+import { getSwapIdentityFromData } from "./src/utils/getSwapIdentityFromData.function";
+import { sendBundledTransactions } from "./src/utils/sendBundledTransactions.function";
 
 let utils = {
     getProgram,
