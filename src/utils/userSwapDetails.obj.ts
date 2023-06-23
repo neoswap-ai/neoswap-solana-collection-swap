6bbb96fb324b4f663afbdb110972406334c24651
biboux-neoswap <biboux.esmail@neoswap.ai>
2023-06-23 19:37:47 +0700

    restructure UserDataInSwap

--- src/utils/userSwapDetails.obj.ts
+++ src/utils/userSwapDetails.obj.ts
@@ -1,5 +1,5 @@
 import { Cluster, PublicKey } from "@solana/web3.js";
-import { ItemStatus, NftSwapItem, SwapData } from "./types";
+import { ItemStatus, NftSwapItem, SwapData, UserDataInSwap } from "./types";
 import { getSwapDataAccountFromPublicKey } from "./getSwapDataAccountFromPublicKey.function";
 import { getProgram } from "./getProgram.obj";
 
@@ -7,21 +7,7 @@ export async function userSwapDetails(Data: {
     cluster: Cluster;
     user: PublicKey;
     swapDataAccount_publicKey: PublicKey;
-}): Promise<{
-    userNftToDeposit: NftSwapItem[];
-    userNftDeposited: NftSwapItem[];
-
-    userNftToReceive: NftSwapItem[];
-    userNftReceived: NftSwapItem[];
-
-    userNftCancelled: NftSwapItem[];
-    userSolCancelled: NftSwapItem[];
-
-    userSolToDeposit: NftSwapItem[];
-    userSolDeposited: NftSwapItem[];
-    userSolToClaim: NftSwapItem[];
-    userSolClaimed: NftSwapItem[];
-}> {
+}): Promise<UserDataInSwap> {
     const program = getProgram(Data.cluster);
     const swapData = await getSwapDataAccountFromPublicKey({
         program,
@@ -36,33 +22,22 @@ export async function userSwapDetails(Data: {
     let userItems = swapData.items.filter((item) => item.owner.equals(Data.user));
     let receiveItems = swapData.items.filter((item) => item.destinary.equals(Data.user));
 
-    let userNftToDeposit = userItems.filter((item) => item.status === ItemStatus.NFTPending);
-    let userNftDeposited = userItems.filter((item) => item.status === ItemStatus.NFTDeposited);
-
-    let userNftToReceive = receiveItems.filter((item) => item.status === ItemStatus.NFTDeposited);
-    let userNftReceived = receiveItems.filter((item) => item.status === ItemStatus.NFTClaimed);
-
-    let userNftCancelled = userItems.filter((item) => item.status === ItemStatus.NFTcanceledRecovered);
-    let userSolCancelled = userItems.filter((item) => item.status === ItemStatus.SolcanceledRecovered);
-
-    let userSolToDeposit = userItems.filter((item) => item.status === ItemStatus.SolPending);
-    let userSolDeposited = userItems.filter((item) => item.status === ItemStatus.SolDeposited);
-    let userSolToClaim = userItems.filter((item) => item.status === ItemStatus.SolToClaim);
-    let userSolClaimed = userItems.filter((item) => item.status === ItemStatus.SolClaimed);
-    
     return {
-        userNftToDeposit,
-        userNftDeposited,
+        userNftToDeposit: userItems.filter((item) => item.status === ItemStatus.NFTPending),
+        userNftDeposited: userItems.filter((item) => item.status === ItemStatus.NFTDeposited),
 
-        userNftToReceive,
-        userNftReceived,
+        userNftToReceive: receiveItems.filter((item) => item.status === ItemStatus.NFTDeposited),
+        userNftReceived: receiveItems.filter((item) => item.status === ItemStatus.NFTClaimed),
+        userNftCancelled: userItems.filter(
+            (item) => item.status === ItemStatus.NFTcanceledRecovered
+        ),
+        userSolCancelled: userItems.filter(
+            (item) => item.status === ItemStatus.SolcanceledRecovered
+        ),
 
-        userNftCancelled,
-        userSolCancelled,
-
-        userSolToDeposit,
-        userSolDeposited,
-        userSolToClaim,
-        userSolClaimed
-    };
+        userSolToDeposit: userItems.filter((item) => item.status === ItemStatus.SolPending),
+        userSolDeposited: userItems.filter((item) => item.status === ItemStatus.SolDeposited),
+        userSolToClaim: userItems.filter((item) => item.status === ItemStatus.SolToClaim),
+        userSolClaimed: userItems.filter((item) => item.status === ItemStatus.SolClaimed),
+    } as UserDataInSwap;
 }
