881c5d311a1e3c7233800c17290618fdb0f6188c
biboux-neoswap <biboux.esmail@neoswap.ai>
2023-06-23 19:38:20 +0700

    can skip simulation and broadcast transaction directly to blockchain

--- src/utils/sendBundledTransactions.function.ts
+++ src/utils/sendBundledTransactions.function.ts
@@ -8,18 +8,18 @@ export async function sendBundledTransactions(Data: {
     txsWithoutSigners: TxWithSigner;
     signer: Keypair;
     cluster: Cluster | string;
+    skipPreflight?: boolean;
 }) {
     try {
         // console.log(txWithSigners);
 
-        let program= getProgram(Data.cluster, Data.signer);
-        const txsWithSigners = await Promise.all(
-            Data.txsWithoutSigners.map((txWithoutSigners) => {
-                txWithoutSigners.signers = [Data.signer];
-                txWithoutSigners.tx.feePayer = Data.signer.publicKey;
-                return txWithoutSigners;
-            })
-        );
+        let program = getProgram(Data.cluster, Data.signer);
+        const txsWithSigners = Data.txsWithoutSigners.map((txWithSigners) => {
+            txWithSigners.signers = [Data.signer];
+            txWithSigners.tx.feePayer = Data.signer.publicKey;
+            return txWithSigners;
+        });
+
         // console.log("txsWithSigners", txsWithSigners);
         console.log(
             "User ",
@@ -31,11 +31,11 @@ export async function sendBundledTransactions(Data: {
         if (!program.provider.sendAll)
             throw { message: "your provider is not an AnchorProvider type" };
 
-        const transactionHashes = await program.provider.sendAll(txsWithSigners, {
-            // skipPreflight: true,
+        const transactionHashs = await program.provider.sendAll(txsWithSigners, {
+            skipPreflight: Data.skipPreflight,
         });
-        console.log(transactionHashes);
-        return { transactionHashes };
+
+        return { transactionHashs };
     } catch (error) {
         throw error;
     }
