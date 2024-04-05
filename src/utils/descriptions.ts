6cc9b78e1c691343ee6d295383967dd634ef79db
biboux-neoswap <biboux.esmail@neoswap.ai>
2024-04-05 22:58:54 +0700

    desc update

--- src/utils/descriptions.ts
+++ src/utils/descriptions.ts
@@ -1,10 +1,19 @@
+// export const DESC = {
+//     makeSwap: "makeSwap",
+//     takeSwap: "takeSwap",
+//     payRoyalties: "payRoyalties",
+//     claimSwap: "claimSwap",
+//     cancelSwap: "cancelSwap",
+//     addBid: "addBid",
+//     rmBid: "rmBid",
+//     setTime: "setTime",
+// };
 export const DESC = {
-    makeSwap: "makeSwap",
-    takeSwap: "takeSwap",
-    payRoyalties: "payRoyalties",
-    claimSwap: "claimSwap",
-    cancelSwap: "cancelSwap",
-    addBid: "addBid",
-    rmBid: "rmBid",
-    setTime: "setTime",
+    makeSwap: "Creating...",
+    takeSwap: "Accepting...",
+    payRoyalties: "Paying royalties...",
+    claimSwap: "Claiming...",
+    cancelSwap: "Canceling...",
+    addBid: "Adding Bid...",
+    removeBid: "Removing Bid...",
 };
