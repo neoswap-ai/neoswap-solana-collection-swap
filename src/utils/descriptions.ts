0208223840bb2142a6ddff8060df30ba18c29233
biboux-neoswap <biboux.esmail@neoswap.ai>
2024-04-08 22:20:10 +0700

    Update descriptions in utils

--- src/utils/descriptions.ts
+++ src/utils/descriptions.ts
@@ -1,20 +1,20 @@
-// export const DESC = {
-//     makeSwap: "makeSwap",
-//     takeSwap: "takeSwap",
-//     payRoyalties: "payRoyalties",
-//     claimSwap: "claimSwap",
-//     cancelSwap: "cancelSwap",
-//     addBid: "addBid",
-//     rmBid: "rmBid",
-//     setTime: "setTime",
-// };
 export const DESC = {
-    makeSwap: "Proposing...",
-    takeSwap: "Accepting...",
-    payRoyalties: "Paying royalties...",
-    claimSwap: "Claiming...",
-    cancelSwap: "Canceling...",
-    addBid: "Adding Bid...",
-    rmBid: "Removing Bid...",
-    setTime: "setting new end date...",
+    makeSwap: "makeSwap",
+    takeSwap: "takeSwap",
+    payRoyalties: "payRoyalties",
+    claimSwap: "claimSwap",
+    cancelSwap: "cancelSwap",
+    addBid: "addBid",
+    rmBid: "rmBid",
+    setTime: "setTime",
 };
+// export const DESC = {
+//     makeSwap: "Proposing...",
+//     takeSwap: "Accepting...",
+//     payRoyalties: "Paying royalties...",
+//     claimSwap: "Claiming...",
+//     cancelSwap: "Canceling...",
+//     addBid: "Adding Bid...",
+//     rmBid: "Removing Bid...",
+//     setTime: "setting new end date...",
+// };
